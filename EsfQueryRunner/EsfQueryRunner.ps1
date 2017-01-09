6e18370e3ede6ea66d32c85409941508709bb6a4
vladotrifonov@gmail.com <vladotrifonov@gmail.com>
2017-01-09 18:06:19 +0000

    EsfQueryRunner - added deployment configuration

---
+++ EsfQueryRunner/EsfQueryRunner.ps1
@@ -0,0 +1,10 @@
+﻿Function Start-EsfQueryRunner {
+    $elasticSearch = "C:\Program Files\elasticsearch-5.1.1\bin\elasticsearch.bat"
+    $elasticSearchConfig = "$PSScriptRoot\EsfQueryRunnerConfig\"
+
+    start-process -FilePath $elasticSearch -ArgumentList "-Epath.conf=$elasticSearchConfig"
+    Start-Sleep 10
+    echo "waited for input idle"
+}
+
+Start-EsfQueryRunner
